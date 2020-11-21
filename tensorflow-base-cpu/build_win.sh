56519319b6dd5861afc45f89e4a37d498383962a
Kai Tietz <ktietz@anaconda.com>
2020-11-21 15:01:12 +0100

    Update windows cpu variant for build

--- tensorflow-base-cpu/build_win.sh
+++ tensorflow-base-cpu/build_win.sh
@@ -5,7 +5,7 @@ set -ex
 # variant specific settings
 if [ ${tflow_variant} == "mkl" ]; then
     export TF_NEED_MKL=1
-    export BAZEL_MKL_OPT="--config=mkl"
+    export BAZEL_MKL_OPT="--config=mkl "
 else
     # eigen variant, do not build with MKL support
     export TF_NEED_MKL=0
@@ -18,7 +18,9 @@ export PYTHON_BIN_PATH="$PYTHON"
 export PYTHON_LIB_PATH="$SP_DIR"
 
 export TF_NEED_CUDA=0
-export TF_ENABLE_XLA=1
+export TF_CUDA_CLANG=0
+export TF_DOWNLOAD_CLANG=0
+export TF_ENABLE_XLA=0
 export TF_NEED_VERBS=0
 export TF_NEED_GCP=1
 export TF_NEED_KAFKA=0
@@ -26,12 +28,14 @@ export TF_NEED_HDFS=0
 export TF_NEED_OPENCL=0
 export TF_NEED_OPENCL_SYCL=0
 export TF_NEED_CLANG=0
+export TF_NEED_ROCM=0
 export TF_NEED_TENSORRT=0
 export TF_SET_ANDROID_WORKSPACE=0
 export TF_DOWNLOAD_CLANG=0
 export TF_NEED_ROCM=0
 export TF_NEED_MPI=0
 export TF_NEED_COMPUTECPP=0
+export TF_CONFIGURE_IOS=0
 unset OLD_PATH
 unset ORIGINAL_PATH
 unset __VSCMD_PREINIT_PATH
@@ -64,6 +68,7 @@ unset STDLIB_DIR
 unset SCRIPTS
 
 # rm -rf .bazelrc
+mv .bazelrc bazelrc_old
 cp -f ${RECIPE_DIR}/def_bazelrc .bazelrc
 echo "" | ./configure
 
@@ -72,7 +77,7 @@ echo "" | ./configure
 # Windows. This can be mitigated by keeping the global build matrix contents to
 # the absolute minimum.
 BUILD_OPTS="--define=override_eigen_strong_inline=true ${BAZEL_MKL_OPT}"
-${LIBRARY_BIN}/bazel --output_base $SRC_DIR/../bazel --batch build -c opt $BUILD_OPTS tensorflow/tools/pip_package:build_pip_package || exit $?
+${LIBRARY_BIN}/bazel --output_base $SRC_DIR/../bazel --batch build -c opt $BUILD_OPTS //tensorflow/tools/pip_package:build_pip_package || exit $?
 
 # xref: https://github.com/tensorflow/tensorflow/issues/21886
 # xref: https://github.com/tensorflow/tensorflow/issues/6396
@@ -82,15 +87,16 @@ ${LIBRARY_BIN}/bazel --output_base $SRC_DIR/../bazel --batch build -c opt $BUILD
 # export _param_file="/c/users/$USER/_bazel_$USER/xxxxxxxx/execroot/org_tensorflow/bazel-out/x64_windows-opt/bin/tensorflow/lite/toco/python/_tensorflow_wrap_toco.so-2.params"
 # while true; do if [ -f $_param_file ]; then sed -i 's,^/WHOLEARCHIVE:\(.*external.*\),\1,' $_param_file; echo done; break; else sleep 1; fi; done
 
-PY_TEST_DIR="py_test_dir"
+PY_TEST_DIR="$SRC_DIR/py_test_dir"
 rm -fr ${PY_TEST_DIR}
 mkdir -p ${PY_TEST_DIR}
-cmd /c "mklink /J ${PY_TEST_DIR}\\tensorflow .\\tensorflow"
+cmd /c "mklink /J $(cygpath -w ${PY_TEST_DIR})\\tensorflow) .\\tensorflow"
 
-./bazel-bin/tensorflow/tools/pip_package/build_pip_package "$PWD/${PY_TEST_DIR}"
+./bazel-bin/tensorflow/tools/pip_package/build_pip_package "$(cygpath -w ${PY_TEST_DIR})"
 
 PIP_NAME=$(ls ${PY_TEST_DIR}/tensorflow-*.whl)
-# python -m pip install ${PIP_NAME} --no-deps -vv --ignore-installed
+# install the whl using pip
+# pip install --no-deps ${PIP_NAME}
 unzip ${PIP_NAME} -d $SP_DIR
 
 # The tensorboard package has the proper entrypoint
