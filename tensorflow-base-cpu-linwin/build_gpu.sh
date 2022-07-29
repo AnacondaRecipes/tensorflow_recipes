12dbc15f07f019a181b4b6fbdf8e8a193b0c42f2
Tobias "Tobi" Koch <tkoch@anaconda.com>
2022-07-29 13:21:03 +0300

    copy gpu build scripts

---
+++ tensorflow-base-cpu-linwin/build_gpu.sh
@@ -0,0 +1,110 @@
+#!/bin/bash
+
+set -vex
+
+# expand PREFIX in BUILD file
+sed -i -e "s:\${PREFIX}:${PREFIX}:" tensorflow/core/platform/default/build_config/BUILD
+
+mkdir -p ./bazel_output_base
+export BAZEL_OPTS=""
+
+# Compile tensorflow from source
+export PYTHON_BIN_PATH=${PYTHON}
+export PYTHON_LIB_PATH=${SP_DIR}
+export USE_DEFAULT_PYTHON_LIB_PATH=1
+
+# make sure this matches configuration of build-machine!
+export CUDA_TOOLKIT_PATH=/usr/local/cuda-10.1
+
+# export PATH="$CUDA_TOOLKIT_PATH/bin:$PATH"
+# export LD_LIBRARY_PATH="$CUDA_TOOLKIT_PATH/lib64 $LD_LIBRARY_PATH"
+# additional settings
+# do not build with MKL support
+export TF_NEED_MKL=0
+export CC_OPT_FLAGS="-march=nocona -mtune=haswell"
+export TF_ENABLE_XLA=1
+export TF_NEED_OPENCL=0
+export TF_NEED_OPENCL_SYCL=0
+export TF_NEED_COMPUTECPP=0
+export TF_NEED_ROCM=0
+export TF_NEED_MPI=0
+export TF_SET_ANDROID_WORKSPACE=0
+export TF_CONFIGURE_IOS=0
+
+# CUDA details
+export TF_NEED_CUDA=1
+export TF_CUDA_VERSION="${cudatoolkit}"
+export TF_CUDNN_VERSION="${cudnn:0:1}"
+export TF_CUDA_CLANG=0
+export TF_DOWNLOAD_CLANG=0
+export TF_NEED_TENSORRT=0
+
+# Additional compute capabilities can be added if desired but these increase
+# the build time and size of the package.
+if [ ${cudatoolkit} == "9.0" ]; then
+    export TF_CUDA_COMPUTE_CAPABILITIES="3.5,5.2,6.0,6.1,7.0"
+fi
+if [ ${cudatoolkit} == "9.2" ]; then
+    export TF_CUDA_COMPUTE_CAPABILITIES="3.5,5.2,6.0,6.1,7.0"
+fi
+if [[ ${cudatoolkit} == 10.* ]]; then
+    export TF_CUDA_COMPUTE_CAPABILITIES="3.5,5.2,6.0,6.1,7.0,7.5"
+fi
+if [[ ${cudatoolkit} == 11.* ]]; then
+      export TF_CUDA_COMPUTE_CAPABILITIES="3.5,5.2,6.0,6.1,7.0,7.5,8.0"
+fi
+export TF_NCCL_VERSION=""
+export GCC_HOST_COMPILER_PATH="${CC}"
+
+# Use system paths here rather than $PREFIX to allow Bazel to find the correct
+# libraries.  RPATH is adjusted post build to link to the DSOs in $PREFIX
+export TF_CUDA_PATHS="${PREFIX},/usr/local/cuda-10.1,/usr"
+
+bazel clean --expunge
+bazel shutdown
+
+./configure
+
+# build using bazel
+# for debugging the following lines may be helpful
+#   --logging=6 \
+#   --subcommands \
+# jobs can be used to limit parallel builds and reduce resource needs
+#    --jobs=20             \
+bazel ${BAZEL_OPTS} build \
+    --copt=-march=nocona \
+    --copt=-mtune=haswell \
+    --copt=-ftree-vectorize \
+    --copt=-fPIC \
+    --copt=-fstack-protector-strong \
+    --copt=-O2 \
+    --cxxopt=-fvisibility-inlines-hidden \
+    --cxxopt=-fmessage-length=0 \
+    --linkopt=-zrelro \
+    --linkopt=-znow \
+    --linkopt="-L${PREFIX}/lib" \
+    --verbose_failures \
+    --config=opt \
+    --config=cuda \
+    --strip=always \
+    --color=yes \
+    --curses=no \
+    --action_env="PYTHON_BIN_PATH=${PYTHON}" \
+    --action_env="PYTHON_LIB_PATH=${SP_DIR}" \
+    --python_path="${PYTHON}" \
+    --define=PREFIX="$PREFIX" \
+    --copt=-DNO_CONSTEXPR_FOR_YOU=1 \
+    --host_copt=-DNO_CONSTEXPR_FOR_YOU=1 \
+    --define=LIBDIR="$PREFIX/lib" \
+    --define=INCLUDEDIR="$PREFIX/include" \
+    //tensorflow/tools/pip_package:build_pip_package
+
+# build a whl file
+mkdir -p $SRC_DIR/tensorflow_pkg
+bazel-bin/tensorflow/tools/pip_package/build_pip_package $SRC_DIR/tensorflow_pkg
+
+# install using pip from the whl file
+${PYTHON} -m pip install --no-deps $SRC_DIR/tensorflow_pkg/*.whl
+
+# The tensorboard package has the proper entrypoint
+rm -f ${PREFIX}/bin/tensorboard
---
+++ tensorflow-base-cpu-linwin/win_helper_gpu.sh
@@ -0,0 +1,147 @@
+#!/bin/bash
+
+set -x
+
+error_exit()
+{
+    kill $1
+    if [ "$?" != "0" ]; then
+        exit 1
+    fi
+}
+
+# expand PREFIX in tensor's build_config/BUILD file
+sed -i -e "s:\${PREFIX}:${PREFIX}:" tensorflow/core/platform/default/build_config/BUILD
+
+export PYTHON_BIN_PATH="$PYTHON"
+export PYTHON_LIB_PATH="$SP_DIR"
+export USE_DEFAULT_PYTHON_LIB_PATH=1
+
+export TF_NEED_CUDA=1
+export TF_CUDA_CLANG=0
+export TF_ENABLE_XLA=1
+export TF_NEED_MKL=0
+export TF_NEED_VERBS=0
+export TF_NEED_GCP=1
+export TF_NEED_KAFKA=0
+export TF_NEED_OPENCL=0
+export TF_NEED_HDFS=0
+export TF_NEED_OPENCL_SYCL=0
+
+export USE_MSVC_WRAPPER=1
+export TF_CUDA_VERSION=${cudatoolkit}
+export CUDA_TOOLKIT_PATH="C:/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v${cudatoolkit}"
+export CUDNN_INSTALL_PATH=$(cygpath -m "$LIBRARY_PREFIX")
+export TF_CUDNN_VERSION=${cudnn:0:1}
+export TF_CUDA_PATHS="${CUDA_TOOLKIT_PATH}"
+export PATH="/c/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v${cudatoolkit}/bin:$PATH"
+export PATH="/c/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v${cudatoolkit}/extras/CUPTI/libx64:$PATH"
+
+export TF_CUDA_COMPUTE_CAPABILITIES="3.0,3.5,5.2"
+if [ ${cudatoolkit} == "8.0" ]; then
+    export TF_CUDA_COMPUTE_CAPABILITIES="3.0,3.5,5.2,6.0,6.1"
+fi
+if [ ${cudatoolkit} == "9.0" ]; then
+    export TF_CUDA_COMPUTE_CAPABILITIES="3.0,3.5,5.2,6.0,6.1,7.0"
+fi
+if [[ ${cudatoolkit} == 10.* ]]; then
+    export TF_CUDA_COMPUTE_CAPABILITIES="3.0,3.5,5.2,6.0,6.1,7.0,7.5"
+fi
+if [[ ${cudatoolkit} == 11.* ]]; then
+    export TF_CUDA_COMPUTE_CAPABILITIES="3.5,5.2,6.0,6.1,7.0,7.5,8.0"
+fi
+
+export TF_NEED_CLANG=0
+export TF_NEED_ROCM=0
+export TF_NEED_TENSORRT=0
+export TF_ANDROID_WORKSPACE=0
+export TF_DOWNLOAD_CLANG=0
+export TF_NEED_MPI=0
+export TF_NEED_COMPUTECPP=0
+export TF_CONFIGURE_IOS=0
+
+unset OLD_PATH
+unset ORIGINAL_PATH
+unset __VSCMD_PREINIT_PATH
+unset ACLOCAL_PATH
+unset WindowsSDK_ExecutablePath_x64
+unset SSH_AUTH_SOCK
+unset SSH_ASKPASS
+unset PSMODULEPATH
+unset PROMPT
+unset PRINTER
+unset PKG_CONFIG_PATH
+unset VS140COMNTOOLS
+unset __VSCMD_PREINIT_INCLUDE
+unset VSINSTALLDIR
+unset VCIDEInstallDir
+unset VS150COMNTOOLS
+unset pin_run_as_build
+unset HTMLHelpDir
+unset FrameworkDir
+unset FrameworkDIR64
+unset PATH_OVERRIDE
+unset INSPECTOR_XE_2016_DIR
+unset INSPECTOR_XE_2013_DIR
+unset INTEL_LICENSE_FILE
+unset ICPP_COMPILER14
+unset ICPP_COMPILER16
+unset CONDA_PROMPT_MODIFIER
+unset CONDA_DEFAULT_ENV
+unset STDLIB_DIR
+unset SCRIPTS
+unset HOME
+unset ICPP_COMPILER19
+unset IFORT_COMPILER14
+unset IFORT_COMPILER16
+unset IFORT_COMPILER19
+unset INFOPATH
+unset INSPECTOR_2019_DIR
+unset INTEL_DEV_REDIST
+unset LIBRARY_LIB
+unset LIBRARY_INC
+unset LIBRARY_PREFIX
+
+bazel clean --expunge
+bazel shutdown
+
+mv .bazelrc bazelrc_old
+cp -f ${RECIPE_DIR}/def_bazelrc .bazelrc
+echo "" | ./configure
+
+cp $RECIPE_DIR/vile_hack.sh ./
+bash vile_hack.sh &
+pid=$!
+
+BUILD_OPTS="--logging=6 --subcommands --define=override_eigen_strong_inline=true --experimental_shortened_obj_file_path=true --define=no_tensorflow_py_deps=true"
+${LIBRARY_BIN}/bazel --output_base $SRC_DIR/../bazel --batch build -c opt $BUILD_OPTS \
+  --action_env="PYTHON_BIN_PATH=${PYTHON}" \
+  --action_env="PYTHON_LIB_PATH=${SP_DIR}" \
+  --linkopt="-L$LIBRARY_PREFIX" \
+  --python_path="${PYTHON}" \
+  --config=cuda --strip=always \
+  --define=PREFIX="${PREFIX}" \
+  --copt=-DNO_CONSTEXPR_FOR_YOU=1 \
+    --host_copt=-DNO_CONSTEXPR_FOR_YOU=1 \
+    --copt=-D_copysign="copysign" \
+    --host_copt=-D_copysign="copysign" --cxxopt=-D_copysign="copysign" \
+    --host_cxxopt=-D_copysign="copysign" --define=LIBDIR="LIBRARY_PREFIX" \
+    --define=INCLUDEDIR="$LIBRARY_INC" \
+  //tensorflow/tools/pip_package:build_pip_package || exit $?
+  error_exit $pid
+
+PY_TEST_DIR="$SRC_DIR/py_test_dir"
+rm -fr ${PY_TEST_DIR}
+mkdir -p ${PY_TEST_DIR}
+cmd //c "mklink /J $(cygpath -w ${PY_TEST_DIR})\\tensorflow .\\tensorflow"
+
+rm -rf /c/t/tmp.XXXXXXXXXX
+mkdir -p /c/t/tmp.XXXXXXXXXX
+./bazel-bin/tensorflow/tools/pip_package/build_pip_package.exe $(cygpath -w ${PY_TEST_DIR})
+
+PIP_NAME=$(ls ${PY_TEST_DIR}/tensorflow-*.whl)
+# $PYTHON -m pip install ${PIP_NAME} --no-deps -vv --ignore-installed
+unzip ${PIP_NAME} -d $SP_DIR
+
+# The tensorboard package has the proper entrypoint
+rm -f ${PREFIX}/Scripts/tensorboard.exe
