f373e500a2963d4e278c9e18348163c62501c7cd
Eric Lundby <Eric.Lundby@gmail.com>
2023-06-12 20:58:16 +0000

    Try removing bash call

--- tensorflow-base-cpu-gpu-linwin/bld.bat
+++ tensorflow-base-cpu-gpu-linwin/bld.bat
@@ -1,31 +1,78 @@
 @echo on
-:: Delegate to the Unixy script. We need to translate the key path variables
-:: to be Unix-y rather than Windows-y, though.
-set "saved_recipe_dir=%RECIPE_DIR%"
-set "saved_source_dir=%SRC_DIR%"
 
-FOR /F "delims=" %%i IN ('cygpath.exe -u "%PYTHON%"') DO set "BAZEL_PYTHON=%%i"
-FOR /F "delims=" %%i IN ('cygpath.exe -u "%LIBRARY_PREFIX%\usr\bin\bash.exe"') DO set "BAZEL_SH=%%i"
+set "PATH=%CD%:%PATH%"
+set LIBDIR=%LIBRARY_BIN%
+set INCLUDEDIR=%LIBRARY_INC%
 
-FOR /F "delims=" %%i IN ('cygpath.exe -u -p "%PATH%"') DO set "PATH_OVERRIDE=%%i"
-FOR /F "delims=" %%i IN ('cygpath.exe -u "%PREFIX%"') DO set "PREFIX=%%i"
-FOR /F "delims=" %%i IN ('cygpath.exe -u "%PYTHON%"') DO set "PYTHON=%%i"
-FOR /F "delims=" %%i IN ('cygpath.exe -u "%RECIPE_DIR%"') DO set "RECIPE_DIR=%%i"
-FOR /F "delims=" %%i IN ('cygpath.exe -u "%SP_DIR%"') DO set "SP_DIR=%%i"
-FOR /F "delims=" %%i IN ('cygpath.exe -u "%SRC_DIR%"') DO set "SRC_DIR=%%i"
-FOR /F "delims=" %%i IN ('cygpath.exe -u "%STDLIB_DIR%"') DO set "STDLIB_DIR=%%i"
+set "TF_SYSTEM_LIBS=llvm,swig"
 
-:: LIBRARY_PREFIX gets translated to '/' instead of the absolute path
-FOR /F "delims=" %%i IN ('cygpath.exe -u "%PREFIX%"') DO set "JAVA_HOME=%%i/Library"
-FOR /F "delims=" %%i IN ('cygpath.exe -u "%PREFIX%"') DO set "LIBRARY_BIN=%%i/Library/bin"
+:: build with MKL support
+set TF_NEED_MKL=1
+@REM set BAZEL_MKL_OPT= 
 
-:: Need a very short TMPDIR otherwise we hit the max path limit while compiling bazel
-FOR /F "delims=" %%i IN ('cygpath.exe -u "%SYSTEMDRIVE%\t"') DO set "TMPDIR=%%i"
+mkdir -p ./bazel_output_base
+set BAZEL_OPTS=
 
-set MSYSTEM=MINGW%ARCH%
-set MSYS2_PATH_TYPE=inherit
-set CHERE_INVOKING=1
-set "BAZEL_VC=%VSINSTALLDIR%VC"
-set "BAZEL_VS=%VSINSTALLDIR%"
-bash -lc "%RECIPE_DIR%"/win_helper_cpu_gpu.sh
-if errorlevel 1 exit 1
+:: the following arguments are useful for debugging
+::    --logging=6
+::    --subcommands
+:: jobs can be used to limit parallel builds and reduce resource needs
+::    --jobs=20
+:: Set compiler and linker flags as bazel does not account for CFLAGS,
+:: CXXFLAGS and LDFLAGS.
+set BUILD_OPTS="--copt=-march=nocona --copt=-mtune=haswell --copt=-ftree-vectorize --copt=-fPIC --copt=-fstack-protector-strong --copt=-O2 --cxxopt=-fvisibility-inlines-hidden --cxxopt=-fmessage-length=0 --linkopt=-zrelro --linkopt=-znow --verbose_failures --config=mkl --config=opt"
+
+set TF_ENABLE_XLA=0
+set BUILD_TARGET="//tensorflow/tools/pip_package:build_pip_package"
+
+:: Python settings
+set PYTHON_BIN_PATH=%PYTHON%
+set PYTHON_LIB_PATH=%SP_DIR%
+set USE_DEFAULT_PYTHON_LIB_PATH=1
+
+:: additional settings
+set CC_OPT_FLAGS="-march=nocona -mtune=haswell"
+set TF_NEED_OPENCL=0
+set TF_NEED_OPENCL_SYCL=0
+set TF_NEED_COMPUTECPP=0
+set TF_CUDA_CLANG=0
+set TF_NEED_TENSORRT=0
+set TF_NEED_ROCM=0
+set TF_NEED_MPI=0
+set TF_DOWNLOAD_CLANG=0
+set TF_SET_ANDROID_WORKSPACE=0
+
+:: try to avoid hangs in configure.py by setting environment
+:: variables whose value might get prompted if missing, see
+:: https://github.com/tensorflow/tensorflow/blob/master/configure.py
+:: (searching for 'get_var' & 'get_from_env_or_user_or_default')
+:: PYTHON_BIN_PATH / CC_OPT_FLAGS / TF_CUDA_CLANG / TF_DOWNLOAD_CLANG
+:: / TF_NEED_ROCM / TF_NEED_TENSORRT set above already
+set "HOST_C_COMPILER=%CC%"
+set "HOST_CXX_COMPILER=%CXX%"
+set "TF_OVERRIDE_EIGEN_STRONG_INLINE=0"
+set "TF_NEED_CUDA=0"
+
+bazel clean --expunge
+bazel shutdown
+
+:: configure step
+COPY %RECIPE_DIR%\def_bazelrc .bazel.rc /Y
+%PYTHON% configure.py
+if %ERRORLEVEL% neq 0 exit 1
+
+:: build using bazel
+bazel %BAZEL_OPTS% build %BUILD_OPTS% %BUILD_TARGET%
+if %ERRORLEVEL% neq 0 exit 1
+
+:: build a whl file
+mkdir -p %SRC_DIR%\\tensorflow_pkg
+bazel-bin\\tensorflow\\tools\\pip_package\\build_pip_package %SRC_DIR%\\tensorflow_pkg
+if %ERRORLEVEL% neq 0 exit 1
+
+:: The tensorboard package has the proper entrypoint
+DEL /f /q %PREFIX%\Scripts\tensorboard.exe
+
+:: make sure we shutdown things again and are releasing locks ...
+bazel clean --expunge
+bazel shutdown
